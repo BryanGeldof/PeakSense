b93a0c70c0ba3c6e690f4617bfb0f3e7ae15e86b
BryanGeldof <78491527+BryanGeldof@users.noreply.github.com>
2026-06-12 11:01:26 +0200

    Update coordinator.py

--- custom_components/peaksense/coordinator.py
+++ custom_components/peaksense/coordinator.py
@@ -6,12 +6,14 @@ class PeakSenseCore:
     def __init__(self):
         self.detector = SpikeDetector()
         self.storage = Storage()
+        self.last_event = None
 
     def process_value(self, value):
         event = self.detector.process(value, datetime.now().isoformat())
 
         if event:
             self.storage.save_event(event)
+            self.last_event = event
             return event
 
         return None
