c2f108719056998a7bd501eff43628464b5530c2
BryanGeldof <78491527+BryanGeldof@users.noreply.github.com>
2026-06-12 14:59:07 +0200

    Update sensor.py

--- custom_components/peaksense/sensor.py
+++ custom_components/peaksense/sensor.py
@@ -2,15 +2,28 @@ from homeassistant.helpers.entity import Entity
 
 DOMAIN = "peaksense"
 
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    core = hass.data.get(DOMAIN)
+    if core is None:
+        return False
+    sensor = PeakSenseLastEventSensor(hass, core)
+    async_add_entities([sensor])
+    return True
+
 class PeakSenseLastEventSensor(Entity):
-    def __init__(self, hass):
+    def __init__(self, hass, core):
         self.hass = hass
+        self._core = core
         self._state = None
         self._attributes = {}
 
     @property
     def name(self):
         return "PeakSense Last Event"
+
+    @property
+    def unique_id(self):
+        return "peaksense_last_event"
 
     @property
     def state(self):
@@ -20,11 +33,8 @@ class PeakSenseLastEventSensor(Entity):
     def extra_state_attributes(self):
         return self._attributes
 
-    def update_from_core(self):
-        core = self.hass.data["peaksense"]["core"]
-
-        event = core.last_event
-
+    def update(self):
+        event = self._core.last_event
         if event:
             self._state = event.get("peak")
             self._attributes = {
@@ -32,5 +42,5 @@ class PeakSenseLastEventSensor(Entity):
                 "end": event.get("end"),
                 "avg": event.get("avg"),
                 "duration": event.get("duration"),
-                "values": event.get("values"),
+                "label": event.get("label", "unknown"),
             }
