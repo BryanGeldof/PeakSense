93346e2faef98a0ebd9fb227e341ef3da8c24225
BryanGeldof <78491527+BryanGeldof@users.noreply.github.com>
2026-06-12 16:28:17 +0200

    Update config_flow.py

--- custom_components/peaksense/config_flow.py
+++ custom_components/peaksense/config_flow.py
@@ -1,3 +1,30 @@
-{
-    "sensor": "sensor.total_power"
-}
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.helpers import selector
+
+from .const import DOMAIN
+
+
+class PeakSenseConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="PeakSense",
+                data=user_input,
+            )
+
+        schema = vol.Schema({
+            vol.Required("sensor"): selector.EntitySelector(
+                selector.EntitySelectorConfig(
+                    domain="sensor"
+                )
+            )
+        })
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=schema,
+        )
