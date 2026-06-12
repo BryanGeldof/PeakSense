136e51a44a33858e5c1eb6e8f3666f2a22b6106d
BryanGeldof <78491527+BryanGeldof@users.noreply.github.com>
2026-06-12 11:01:55 +0200

    Update __init__.py

--- custom_components/peaksense/__init__.py
+++ custom_components/peaksense/__init__.py
@@ -1,14 +1,34 @@
+from homeassistant.helpers import discovery
 from .coordinator import PeakSenseCore
-from .sensor import PeakSenseSensor
+from .sensor import PeakSenseLastEventSensor
 
 async def async_setup(hass, config):
+
     core = PeakSenseCore()
 
-    sensor = PeakSenseSensor(core)
+    sensor = PeakSenseLastEventSensor(hass)
 
     hass.data["peaksense"] = {
         "core": core,
         "sensor": sensor
     }
+
+    async def handle_update(call):
+        value = float(call.data.get("value", 0))
+        event = core.process_value(value)
+
+        sensor.update_from_core()
+
+        hass.states.async_set(
+            "sensor.peaksense_last_event",
+            sensor.state,
+            sensor.extra_state_attributes
+        )
+
+    hass.services.async_register(
+        "peaksense",
+        "update",
+        handle_update
+    )
 
     return True
