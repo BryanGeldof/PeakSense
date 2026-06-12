aa34445d36810dbf23cd2c57ab9ece080b9fb706
BryanGeldof <78491527+BryanGeldof@users.noreply.github.com>
2026-06-12 09:53:26 +0200

    Create coordinator.py

---
+++ custom_components/peaksense/coordinator.py
@@ -0,0 +1,17 @@
+from .detector import SpikeDetector
+from .storage import Storage
+from datetime import datetime
+
+class PeakSenseCore:
+    def __init__(self):
+        self.detector = SpikeDetector()
+        self.storage = Storage()
+
+    def process_value(self, value):
+        event = self.detector.process(value, datetime.now().isoformat())
+
+        if event:
+            self.storage.save_event(event)
+            return event
+
+        return None
