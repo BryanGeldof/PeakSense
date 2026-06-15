b0eb452f1826021f5925d1279e5ed940fd05b362
BryanGeldof <78491527+BryanGeldof@users.noreply.github.com>
2026-06-15 10:41:47 +0200

    Update config_flow.py

--- custom_components/peaksense/config_flow.py
+++ custom_components/peaksense/config_flow.py
@@ -1,30 +1,18 @@
-import voluptuous as vol
-
 from homeassistant import config_entries
-from homeassistant.helpers import selector
-
+from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
 
 class PeakSenseConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle the config flow for PeakSense."""
+
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        """Handle a user initiated config flow."""
         if user_input is not None:
-            return self.async_create_entry(
-                title="PeakSense",
-                data=user_input,
-            )
+            await self.async_set_unique_id(DOMAIN)
+            self._abort_if_unique_id_configured()
+            return self.async_create_entry(title="PeakSense", data={})
 
-        schema = vol.Schema({
-            vol.Required("sensor"): selector.EntitySelector(
-                selector.EntitySelectorConfig(
-                    domain="sensor"
-                )
-            )
-        })
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=schema,
-        )
+        return self.async_show_form(step_id="user")
